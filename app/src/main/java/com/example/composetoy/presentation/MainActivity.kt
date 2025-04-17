f13f844845a09197a142a0354880c07172a38a7f
CHOIMINSEOK <cms3718@gmail.com>
2025-04-17 15:06:06 +0900

    init mvi

--- app/src/main/java/com/example/composetoy/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composetoy/presentation/MainActivity.kt
@@ -5,18 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
+import org.orbitmvi.orbit.compose.collectAsState
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        viewModel.getList()
         setContent {
+            Box(modifier = Modifier.systemBarsPadding()) {
+                val state by viewModel.collectAsState()
+                Render(state)
+            }
         }
+    }
+
+    @Composable
+    private fun Render(state: MainUiState) {
+        Text(text = state.rawString)
     }
 }
--- app/src/main/java/com/example/composetoy/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/composetoy/presentation/MainViewModel.kt
@@ -4,18 +4,36 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.composetoy.domain.ContentListRepository
+import com.example.composetoy.domain.model.Section
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
+
+data class MainUiState(
+    val rawString: String,
+)
+
+sealed class MainSideEffect
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: ContentListRepository
-): ViewModel() {
+): ViewModel(), ContainerHost<MainUiState, MainSideEffect> {
 
-    fun getList()  {
+    override val container = container<MainUiState, MainSideEffect>(MainUiState(""))
+
+    init {
         viewModelScope.launch {
-            Log.d("TTT", repository.getList().toString())
+            val sectionList = repository.getList()
+            initializeUI(sectionList)
+        }
+    }
+
+    private fun initializeUI(sectionList: List<Section>) = intent {
+        reduce {
+            state.copy(rawString = sectionList.toString())
         }
     }
 }
